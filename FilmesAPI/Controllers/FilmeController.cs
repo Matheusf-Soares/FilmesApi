7ae01c3ae6f83f28b13c1649b5d0e583012887f7
Matheusf-Soares <matheusfps27@gmail.com>
2023-11-01 11:40:25 -0300

    Adição de nova entidade e controller de filme

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,26 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : Controller
+{
+    public static List<Filme> Filmes = new List<Filme>();
+    public static int id = 0;
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        Filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes()
+    {
+        return Filmes;
+    }
+}
